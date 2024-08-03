24d45fbda428b01381a6460167f2079187e73f7a
Nick Koukis <nikoskoukis99@gmail.com>
2024-08-03 21:45:16 +0300

    server details

--- api/server.js
+++ api/server.js
@@ -15,23 +15,23 @@ const path = require('path'); // Import path module for serving static files
 const app = express();
 const PORT = process.env.PORT || 80;
 
-// Connect to MongoDB
-connectDB();
+// // Connect to MongoDB
+// connectDB();
 
-// Use CORS with specific origins
-app.use(cors({
-    origin: 'http://localhost:4200', // Allow requests from Angular frontend
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
-    credentials: true // Enable cookies, authorization headers, and TLS client certificates
-}));
+// // Use CORS with specific origins
+// app.use(cors({
+//     origin: 'http://localhost:4200', // Allow requests from Angular frontend
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify the methods you want to allow
+//     credentials: true // Enable cookies, authorization headers, and TLS client certificates
+// }));
 
-// Middleware to parse JSON request bodies
-app.use(express.json());
+// // Middleware to parse JSON request bodies
+// app.use(express.json());
 
-app.use('/assets/uploads', express.static(path.join(__dirname, 'assets', 'uploads')));
+// app.use('/assets/uploads', express.static(path.join(__dirname, 'assets', 'uploads')));
 
-// Swagger UI setup
-app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+// // Swagger UI setup
+// app.use('/api/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // Root route
 app.get('/', (req, res) => {
@@ -39,14 +39,14 @@ app.get('/', (req, res) => {
 });
 
 // Define authentication routes
-app.use('/api/auth', authRoutes);
+// app.use('/api/auth', authRoutes);
 
 
-app.use('/api/dashboard', dashboardRoutes);
-app.use('/api/customers', customerRoutes);
-app.use('/api/books', bookRoutes);
-app.use('/api/order', orderRoutes);
-app.use('/api/statistics', statisticsRoutes);
+// app.use('/api/dashboard', dashboardRoutes);
+// app.use('/api/customers', customerRoutes);
+// app.use('/api/books', bookRoutes);
+// app.use('/api/order', orderRoutes);
+// app.use('/api/statistics', statisticsRoutes);
 
 
 
